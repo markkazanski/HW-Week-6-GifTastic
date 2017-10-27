2836ed428870f21c9d2359b37fc4b9a981afcde0
Mark Kazanski <markkazanski@gmail.com>
2017-10-26 20:57:54 -0500

    finished and styled

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -16,6 +16,7 @@ var giftastic = {
         this.displayTopics();
         this.addTopic();
         this.getButtonClicks();
+        this.getGifClicks();
     },
 
     topicsArray: [
@@ -60,7 +61,7 @@ var giftastic = {
     },
 
     callGiphy: function( query ){
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=10&q="
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=10&rating=pg-13&q="
          + query;
          console.log(queryURL);
         
@@ -68,19 +69,52 @@ var giftastic = {
             url: queryURL,
             method: 'GET'
         }).done(function(response) {
+            $("#gifs-container").empty();
             for(var i=0; i<response.data.length; i++){
                 console.log(response.data[i].slug);
+                var div = $("<div>"); //create gif container, 
+                div.attr("class", "gif-container");
+                var img = $("<img>");
+                img.attr("src", response.data[i].images.original_still.url);
+                img.attr("data-still", response.data[i].images.original_still.url);
+                img.attr("data-animate", response.data[i].images.original.url);
+                img.attr("data-state", "still");
+                var rating = $("<p>");
+                rating.text(response.data[i].rating);
+                rating.attr("class", "rating");
+                div.append(img);
+                div.append(rating);
+                $("#gifs-container").append(div);
+                //create image object, add src, data-still, data-animate
             }
             console.log(response); //display gifs
+
         });
     },
 
     getButtonClicks: function(){
-        $(".topic-button").on("click", function(){
+        $(document).on("click", ".topic-button", function(){
             var topic = $(this).attr("data-topic"); //get topic from clicked button
             
             giftastic.callGiphy(topic); //use ajax to query Giphy API
         });
+    },
+
+    getGifClicks: function(){
+        $(document).on("click", ".gif-container img", function() {
+            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+            var state = $(this).attr("data-state");
+            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+            // Then, set the image's data-state to animate
+            // Else set src to the data-still value
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            } else {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
+          });
     },
 };
 
